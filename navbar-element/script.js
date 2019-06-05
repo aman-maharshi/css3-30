366198895db37e98e0400fc363b00df05df77da7
aman-maharshi <amanm4harshi@gmail.com>
2019-06-05 23:33:41 +0530

    styling changes

--- navbar-element/script.js
+++ navbar-element/script.js
@@ -1,3 +1,11 @@
 document.querySelector('#toggle').addEventListener('click', (e) => {
     // console.log(e.target.checked)
 })
+
+document.getElementById('landing').addEventListener('click', hideNav);
+
+function hideNav() {
+    // console.log('Click');
+    // document.getElementById('nav-bar').style.display = 'none';
+
+}
